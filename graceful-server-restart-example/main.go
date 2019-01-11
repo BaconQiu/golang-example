f713bbaa17a8f90d606c6df7555c47c2b6261874
BaconQiu <704132895@qq.com>
2019-01-11 11:39:02 +0800

    Delete srvCfg struct in main.go

--- graceful-server-restart-example/main.go
+++ graceful-server-restart-example/main.go
@@ -7,25 +7,6 @@ import (
 	"time"
 )
 
-//var cfg *srvCfg
-
-type srvCfg struct {
-	// Socket file location
-	sockFile string
-
-	// Listen address
-	addr string
-
-	// Listener
-	ln net.Listener
-
-	// Amount of time allowed for requests to finish before server shutdown
-	shutdownTimeout time.Duration
-
-	// Amount of time allowed for a child to properly spin up and request the listener
-	childTimeout time.Duration
-}
-
 func main() {
 	initCfg("/tmp/api.sock", ":8000")
 
